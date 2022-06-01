27d167b5eae99988425865f1cf91cc9903aa31ee
gnimoh <gideon.nimoh@pwc.com>
2022-06-01 06:36:43 +0200

    added new goal list

--- criteria.py
+++ criteria.py
@@ -9,6 +9,7 @@ from utils.utility import Utility
 class Criteria(Utility):
     def __init__(self, profile) -> None:
         super().__init__()
+        self.show_available_data = True
         self.id = profile.profile_id
         self.profile = profile
         self.goals = []
@@ -17,97 +18,111 @@ class Criteria(Utility):
         self.profiles = []
         self.selected_options = []
         self.match = None
-        self.goal_list = ['Getting to know new communities in PwC',
-                          'Working more x-LoS with PwC', 'Fostering an inclusive working environment']
-        self.interest_list = ['Home', 'Movies',
-                              'Sports and outdoors', 'Meditation', 'Scaleups']
-        self.account_info_list = ['Management Level',
-                                  'Start year', 'Gender', 'Birth year']
+        self.goal_list = ['Fostering an inclusive working environment',
+                          'Looking for a better fit on my skills and talent',
+                          'Maintaining relationships',
+                          'Boosting my wellbeing & mental health',
+                          'Making my work more meaningful',
+                          'Joining a group',
+                          'Finding people to join my group',
+                          'Making my work more social and fun',
+                          'Working with different people',
+                          'Working more x-LoS',
+                          'Getting new opportunities',
+                          'Gaining visibility',
+                          'Achieving career development goal',
+                          'Being mentored',
+                          'Getting advised',
+                          'Needing help',
+                          'Gaining new knowledge',
+                          'Getting to know new communities in PwC',
+                          'Being more open to new viewpoints'
+                          ]
+        self.interest_list = ['Scaleups', 'Startups', 'Yoga', 'Weight training', 'Running',
+                              'Physical fitness', 'Physical exercise', 'Meditation', 'Bodybuilding',
+                              'Sports and outdoors', 'Outdoor recreation', 'Movies', 'Live events', 
+                              'Games', 'Vehicles', 'Travel', 'Social issues', 'Politics', 
+                              'Pets', 'Garden', 'Home', 'Music', 'Arts'  
+                              ]
+        self.account_info_list = ['Management Level', 'Start year', 'Gender', 'Birth year']
 
     def get_user_choice(self):
         return input('Your choice: ')
 
-    def add_account_data(self, index):
-        if int(index) not in self.selected_options and len(self.selected_options) != 4:
-            user_input = input(Fore.CYAN + self.account_info_list[index - 1] + ': ')
-            self.account_data.insert(index, user_input)
-            self.selected_options.append(index)
+    def add_profile_data(self, data=None, profile_data=None, account=False):
+        index = self.get_user_choice()
+        if account is True:
+            try: 
+                if int(index) not in self.selected_options and len(self.selected_options) != len(data):
+                    user_input = input(Fore.CYAN + data[int(index) - 1] + ': ')
+                    if profile_data is not None:
+                        profile_data.insert(int(index), user_input)
+                    self.selected_options.append(int(index))
+                else:
+                    print(Fore.RED + 'Option [ {} ] has already been selected!'.format(index))
+                    return
+            except ValueError:
+                return
         else:
-            print(Fore.RED + 'Option [ {} ] has already been selected!'.format(index))
-            return
+            selected_option = int(index) - 1
+            profile_data.insert(selected_option, data[selected_option])
+            # print(f'{Fore.CYAN}You have selected: {data[int(index) - 1]}')
+            print(profile_data)
 
     def add_goal_data(self, index):
         if int(index) not in self.selected_options:
             pass
 
-    def show_data(self, item_list, title):
-        print(Fore.BLUE + 'Select a {} from the list below:'.format(title))
+    def show_data(self, item_list, title, data=None, account=False):
+        print(Fore.BLUE + f'Select a {title} from the list below:')
         for i, item in enumerate(item_list):
             print(Fore.BLUE + '{}: {}'.format(i + 1, item))
         print('q: Continue!')
         print("=" * 50 + '\n')
-
-    def get_goals(self) -> None:
-        show_available_goals = True
-        while show_available_goals:
-            self.show_data(item_list=self.goal_list, title='goal')
-            user_choice = self.get_user_choice()
-            if user_choice == '1':
-                self.goals.append('Getting to know new communities in PwC')
-            elif user_choice == '2':
-                self.goals.append('Working more x-LoS with PwC')
-            elif user_choice == '3':
-                self.goals.append('Fostering an inclusive working environment')
-            elif user_choice == 'q':
-                self.profile.set_goals(data=self.goals)
-                show_available_goals = False
-        else:
-            print(Fore.GREEN + 'Goals added!' + '\n')
+        self.add_profile_data(data=item_list, profile_data=data, account=account)
 
     def get_account_data(self) -> None:
-        show_available_account_data = True
-        while show_available_account_data:
-            self.show_data(item_list=self.account_info_list, title='account_data')
-            user_choice = self.get_user_choice()
-            if user_choice == '1':
-                self.add_account_data(1)
-            elif user_choice == '2':
-                self.add_account_data(2)
-            elif user_choice == '3':
-                self.add_account_data(3)
-            elif user_choice == '4':
-                self.add_account_data(4)
-            elif user_choice == 'q' or len(self.selected_options) == 4:
-                self.profile.set_up_account(data=self.account_data)
-                show_available_account_data = False
-                self.selected_options = []
-            else:
-                print(Fore.RED + 'Option not recognized, please select from list below! or from [1, 2, 3, 4, q]')
-                continue
-        else:
-            print(Fore.GREEN + 'Account set up is completed!' + '\n')
+        while len(self.account_data) != len(self.account_info_list):
+            self.show_data(item_list=self.account_info_list, title='account_data', data=self.account_data, account=True)
+        
+    def get_goals(self) -> None:
+        while len(self.goals) <= 3:
+            self.show_data(item_list=self.goal_list, title='goal')
+        #     user_choice = self.get_user_choice()
+        #     if user_choice == '1':
+        #         self.goals.append('Getting to know new communities in PwC')
+        #     elif user_choice == '2':
+        #         self.goals.append('Working more x-LoS with PwC')
+        #     elif user_choice == '3':
+        #         self.goals.append('Fostering an inclusive working environment')
+        #     elif user_choice == 'q':
+        #         self.profile.set_goals(data=self.goals)
+        #         show_available_goals = False
+        # else:
+        #     print(Fore.GREEN + 'Goals added!' + '\n')
+
 
     def get_interests(self) -> None:
         show_available_interest = True
 
         while show_available_interest:
-            self.show_data(item_list=self.interest_list, title='interests')
-            user_choice = self.get_user_choice()
-            if user_choice == '1':
-                self.interests.append('Home')
-            elif user_choice == '2':
-                self.interests.append('Movies')
-            elif user_choice == '3':
-                self.interests.append('Sports and outdoors')
-            elif user_choice == '4':
-                self.interests.append('Meditation')
-            elif user_choice == '5':
-                self.interests.append('Scaleups')
-            elif user_choice == 'q':
-                self.profile.set_interests(data=self.interests)
-                show_available_interest = False
-        else:
-            print(Fore.GREEN + 'Interests added!' + '\n')
+            self.show_data(item_list=self.interest_list, title='interest')
+        #     user_choice = self.get_user_choice()
+        #     if user_choice == '1':
+        #         self.interests.append('Home')
+        #     elif user_choice == '2':
+        #         self.interests.append('Movies')
+        #     elif user_choice == '3':
+        #         self.interests.append('Sports and outdoors')
+        #     elif user_choice == '4':
+        #         self.interests.append('Meditation')
+        #     elif user_choice == '5':
+        #         self.interests.append('Scaleups')
+        #     elif user_choice == 'q':
+        #         self.profile.set_interests(data=self.interests)
+        #         show_available_interest = False
+        # else:
+        #     print(Fore.GREEN + 'Interests added!' + '\n')
 
     def show_matching_options(self) -> None:
         print(Fore.BLUE + 'Select a matching option:')
--- match.py
+++ match.py
@@ -12,15 +12,17 @@ class Match(Utility):
         self.interest_weight = 0.17
         self.account_weight = 0.33
     
+    
+    
     def calculate_all_attribute_weight(self, list) -> float:
         """
         Total: Weight = 1.0 + 3.0 + 2.0 = 6.0 
         ---------------------------------------------------------------------
-        Goals: Weight = 3.0 / 6.0 = 0.5         [number of matched goals]
+        Goals: Weight = 3.0 / 6.0 = 0.50         [number of matched goals]
         Interests: Weight = 1.0 / 6.0 = 0.17    [number of matched interests]
         Account: Weight = 2.0 / 6.0 = 0.33      [number of matched accounts]
         """
-        all_attribute_count = []
+        # all_attribute_count = []
         for match in list:
             total_weight = 0
             try:
@@ -33,21 +35,25 @@ class Match(Utility):
                 match['total_weight'] =  total_weight
             except KeyError:
                 continue
-        # for x in list:
-        #     if x["profile_id"] == list[0]["profile_id"]:
-        #         continue
-        #     print(Fore.YELLOW + str(x) + '\n')
-    
     
     def calculate_weight(self, attribute_count, total_count) -> float:
         return round((attribute_count / total_count) * 100, 0)
+    
+    
+    def calculate_interest_weight() -> float:
+        pass
+         # For interest -- each should have its own weight 1 - 2 - 3 ==> 1, 3 , 10
+    #    (1/10) * 0.17 === 0.017
+      
+      
+      
+      
         
     def get_profile_data(self, profile_id) -> dict:
         for profile in self.data:
             if profile['profile_id'] == profile_id:
                 return profile
         return {}
-
 
     def start_single_profile_match(self, profile) -> None:
         current_profile_goals = len(profile.__dict__['goals'].goals)
@@ -58,8 +64,10 @@ class Match(Utility):
         self.match_manager(title='account', current_profile=profile.__dict__)
         
         all_matches = self.get_match_list()
-        print(Fore.GREEN + 'Active Profile [Goals: {}, Interests: {}, Account: {}]'.format(current_profile_goals,current_profile_interests, current_profile_account))
+        print(Fore.GREEN + 'Active Profile [Goals: {}, Interests: {}, Account: {}]'.format(current_profile_goals,current_profile_interests, current_profile_interests))
         preferred_match_list = [x for x in all_matches if x['matched_goals_count'] >= current_profile_goals or x['matched_interests_count'] >= current_profile_interests or x['matched_account_count'] >= current_profile_account]
+        self.calculate_all_attribute_weight(list=preferred_match_list)
+        # self.print_top_6_matches(list=preferred_match_list, profile=profile)
         print(("=" * 100) + '\n')
         self.calculate_all_attribute_weight(list=preferred_match_list)
         sorted_weight = sorted(preferred_match_list, key=lambda k: k['total_weight'], reverse=True)
@@ -70,7 +78,6 @@ class Match(Utility):
                 print(Fore.CYAN + str(x))
         print(("=" * 100) + '\n')
         print(Fore.GREEN + 'Total matched profiles: {}'.format(len(preferred_match_list)))
-    
     
     def get_matched_profiles_data(self) -> list:
        for data in [self.get_profile_data(x['profile_id']) for x in self.match_list]:
@@ -134,14 +141,11 @@ class Match(Utility):
         if title == 'account':
             print(Fore.GREEN + 'Completed matching accounts.')
     
-    
     def get_list(self, title, list) -> list:
         return list.goals if title == 'goals' else list.interests if title == 'interests' else list.account
-    
 
     def get_inner_list(self, title, profile_t) -> list:
         return profile_t.goals if title == 'goals' else profile_t.interests if title == 'interests' else profile_t.account if title == 'account' else []
-        
    
     def get_match_list(self) -> list:
         list = []
@@ -152,7 +156,6 @@ class Match(Utility):
              except KeyError:
                 pass
         return list
-
 
     def check_matches_for_all_profiles(self) -> None:
         counter = 0
@@ -182,8 +185,6 @@ class Match(Utility):
             self.match_list = []
             counter += 1
         print(Fore.YELLOW + 'Completed matching profiles: {}'.format(counter))
-        
-    
     
     def print_top_6_matches(self, list, profile) -> None:
         sorted_weight = sorted(list, key=lambda k: k['total_weight'], reverse=True)
@@ -193,6 +194,27 @@ class Match(Utility):
             else:
                 print(Fore.CYAN + str(x) + '\n')
 
+    def check_goal_compatibility(self, profile) -> None:
+        # Goals can be a total of '3' (Weight == 3 == 50%)
+        # total number of goals can be three or less
+        
+        # profile('a', 'b')
+        # profile_2('b', 'c', 'd') -- match of 1 goal
+        
+        # profile('a', 'b')
+        # profile_2('b', 'c', 'd') -- compatible of 1 goal
+        
+        # 75%
+        
+        pass
+        # TODO: get the list of goals for profile and perform a check on other profiles;
+        # If current_profile is looking for someone to coach for example - then it is compatible with any profile that is looking for a coach (Ques: what then is the weight given to a scenario like this)
+        # Have a category of list of goals and if an item from this category is for any two profiles we can say they are compatible
+        # Matched == 1
+        # Unmatched
+        # Compatible == 2
+        
+        
 
 # match = Match()
 # match.check_matches_for_all_profiles()
